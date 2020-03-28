fee3c1420276de5a1db0f507f24e0a5bc483d8b2
s16878 <61700945+s16878@users.noreply.github.com>
2020-03-28 22:48:19 +0100

    Add files via upload

---
+++ StudEnroll.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw4.Controllers.Models
+{
+    public class StudEnroll
+    {
+        /*
+        public string IndexNumber { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string BirthDate { get; set; }
+        */
+        public string IdEnrollment { get; set; }
+        public string Semester { get; set; }
+        public string IdStudy { get; set; }
+        public string StartDate { get; set; }
+    }
+}
